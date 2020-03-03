1e6a1bb6ad61b990b78615b7dacc5bec117447b1
CF <lawchunfu@gmail.com>
2020-03-03 18:09:10 +0800

    Optimized for mobile and dynamic resizing

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 npm install -g create-react-app\
 create-react-app chunfu-personal-website\
-npm install webfontloader --save
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Portrait from "../images/test_portrait.png";
 import "../css/main.css";
 
 export default class Home extends Component {
   render() {
     return (
-        <div className="welcome">
-          <div className="foldpg" />
-          <div className="name">
-            <img
-              className="portrait-img" src={Portrait} alt="portrait"/>
-            <p className="underline">LAW</p>
-            <p> CHUN FU</p>
-          </div>
+      <div className="welcome">
+        <div className="foldpg" />
+        <div className="name">
+          <img className="portrait-img" src={Portrait} alt="portrait" />
+          <p className="underline">LAW</p>
+          <p> CHUN FU</p>
+        </div>
+        <div className="footer">
           <div className="arrow" />
           <div className="socialmedia">
             <a href="#" className>
@@ -25,9 +25,10 @@ export default class Home extends Component {
               <i className="fa fa-envelope" />
             </a>
           </div>
-          <div className="shadow" />
         </div>
+        <div className="shadow" />
+      </div>
+
     );
   }
 }
-
